84a09352bcd7c2fb651845a688a27de0857a7b96
osyduckInc <160811986+osyduckInc@users.noreply.github.com>
2024-02-22 11:19:06 +0700

    Update rasp.py

--- rasp.py
+++ rasp.py
@@ -1,46 +1,88 @@
-from picamera import PiCamera
 import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+import pyfirmata
+import numpy as np
+import time
 
-# Inisiasi kamera
-camera = PiCamera()
+cap = cv2.VideoCapture(1)
+ws, hs = 1280, 720
+cap.set(3, ws)
+cap.set(4, hs)
 
-# Set resolusi kamera 
-camera.resolution = (640, 480)
+if not cap.isOpened():
+    print("Camera couldn't Access!!!")
+    exit()
 
-# Set framerate kamera
-camera.framerate = 32
 
-# Membuka preview kamera
-camera.start_preview() 
+port = "COM4"
+board = pyfirmata.Arduino(port)
+servo_pinX = board.get_pin('d:9:s') #pin 9 Arduino
+servo_pinY = board.get_pin('d:10:s') #pin 10 Arduino
+led_pin = board.get_pin('d:7:o') # Pin 7 Arduino
 
-# Inisiasi haar cascade untuk deteksi wajah
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detector = FaceDetector()
+servoPos = [90, 90] # initial servo position
 
-# Loop untuk membaca frame secara realtime
+servoLook = servoPos[0]
+lookServo = 90
+
+print(type(servoLook))
+
 while True:
+    success, img = cap.read()
+    img, bboxs = detector.findFaces(img, draw=False)
 
-  # Ambil frame dari kamera
-  camera.capture(rawCapture, format="bgr")
-  image = rawCapture.array
+    if bboxs:
 
-  # Ubah ke grayscale
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  
-  # Deteksi wajah
-  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        led_pin.write(1)
+        #get the coordinate
+        fx, fy = bboxs[0]["center"][0], bboxs[0]["center"][1]
+        pos = [fx, fy]
+        #convert coordinate to servo degree
+        servoX = np.interp(fx, [0, ws], [180, 0])
+        servoY = np.interp(fy, [0, hs], [180, 0])
+        if servoX < 0:
+            servoX = 0
+        elif servoX > 180:
+            servoX = 180
+        if servoY < 0:
+            servoY = 0
+        elif servoY > 180:
+            servoY = 180
 
-  # Gambar kotak pada wajah
-  for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2) 
+        servoPos[0] = servoX
+        servoPos[1] = servoY
 
-  # Tampilkan hasil    
-  cv2.imshow('frame', image)
 
-  # Jeda 10ms sebelum ambil frame berikutnya
-  key = cv2.waitKey(10)
-  if key == 27:
-    break
+        cv2.circle(img, (fx, fy), 80, (0, 0, 255), 2)
+        cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2 )
+        cv2.line(img, (0, fy), (ws, fy), (0, 0, 0), 2)  # x line
+        cv2.line(img, (fx, hs), (fx, 0), (0, 0, 0), 2)  # y line
+        cv2.circle(img, (fx, fy), 15, (0, 0, 255), cv2.FILLED)
+        cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3 )
 
-# Membersihkan dan stop kamera
-cv2.destroyAllWindows()
-camera.stop_preview()
+    else:
+        lookServo += 180
+        if lookServo > 180:
+            lookServo = 0
+        servo_pinX.write(lookServo)
+        time.sleep(1)
+
+        led_pin.write(0)
+        cv2.putText(img, "NO TARGET", (880, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        cv2.circle(img, (640, 360), 80, (0, 0, 255), 2)
+        cv2.circle(img, (640, 360), 15, (0, 0, 255), cv2.FILLED)
+        cv2.line(img, (0, 360), (ws, 360), (0, 0, 0), 2)  # x line
+        cv2.line(img, (640, hs), (640, 0), (0, 0, 0), 2)  # y line
+
+
+    cv2.putText(img, f'Servo X: {int(servoPos[0])} deg', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(img, f'Servo Y: {int(servoPos[1])} deg', (50, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+
+    servo_pinX.write(servoPos[0])
+    servo_pinY.write(servoPos[1])
+    
+    
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
